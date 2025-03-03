caa410d3b188e1de229a3e21286b98b365264f06
BrianSuarez <Brian.suarez0112@hotmail.com>
2025-03-03 17:58:52 -0500

    fix: :bug: Correction Error Validation of credentials
    An error has been corrected when validating credentials that only validated the first user. It has also been corrected so that it depends on the information of the 3 types of errors as appropriate.

--- js/credenciales.js
+++ js/credenciales.js
@@ -6,10 +6,12 @@ const ENV = async()=>{
 export const validation = (users, data) => {
     for (let i = 0; i < users.length; i++) {
         if (users[i].USER === data.email){
-            const {USER:email_user, ROL:type_user} = users[i];
-            return {status:200, email_user, type_user};
+            if (users[i].PWD === data.password){
+                const {USER:email_user, ROL:type_user} = users[i];
+                return {status:200, email_user, type_user};
+            }
+            else return {status: 401, message: "Incorrect credentials"};
         }
-        else return {status: 401, message: "Incorrect credentials"};
     }
     return {status:404, message:"User not found"};
 }
