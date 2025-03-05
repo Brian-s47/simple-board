e2cc610c4d1309aee6a24d157636fab19e677048
BrianSuarez <Brian.suarez0112@hotmail.com>
2025-03-05 15:01:04 -0500

    feat: :construction: Modulation data calculation
    Data calculation modules are added to display in page information

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,14 +1,18 @@
-import {finAllStudent} from "./students.js";
-import {finAllCourse} from "./course.js";
-import {finAllPayments} from "./payments.js";
+import {findAllStudent} from "./students.js";
+import {findAllCourse} from "./course.js";
+import {findAllPayments} from "./payments.js";
+import {findAllUsers} from "./user.js";
 
-const students = document.querySelector("#students > strong")
-const course = document.querySelector("#course > strong")
-const payment = document.querySelector("#payments > strong")
 
-students.innerHTML = finAllStudent()
-course.innerHTML = finAllCourse()
+const students = document.querySelector("#students > strong");
+const course = document.querySelector("#course > strong");
+const payment = document.querySelector("#payment > strong");
+const user = document.querySelector("#user > strong");
 
-const money = JSON.parse(finAllPayments());
+students.innerHTML = findAllStudent()
+course.innerHTML = findAllCourse()
+user.innerHTML = findAllUsers()
+
+const money = JSON.parse(findAllPayments());
 payment.dataset.format = money.format;
 payment.innerHTML = money.total;
